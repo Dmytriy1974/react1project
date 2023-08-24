0e2071e40349e84b4c213646c260e6859586061d
Dmytro <dmii3iy@gmail.com>
2023-08-24 23:42:27 +0300

    disappered Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,31 @@
 import s from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom'
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.abonents}>
                 <div className={s.abonentsItem}>
-                <NavLink to="/dialogs/1"> Dimych</NavLink>
+                    <NavLink to="/dialogs/1" className={ ({isActive})=> isActive? s.active : ''}> Dimych</NavLink>
                 </div>
-                <div className={s.abonentsItem}> Andrey</div>
-                <div className={s.abonentsItem}> Sveta</div>
-                <div className={s.abonentsItem}> Sasha</div>
-                <div className={s.abonentsItem}> Victor </div>
-                <div className={s.abonentsItem}>Valera </div>
+                <div className={s.abonentsItem}>
+                    <NavLink to="/dialogs/2"> Andrey</NavLink>
+                </div>
+                <div className={s.abonentsItem}>
+                    <NavLink to="/dialogs/3"> Sveta</NavLink>
+                </div>
+                <div className={s.abonentsItem}>
+                    <NavLink to="/dialogs/4"> Sasha</NavLink>
+                </div>
+                <div className={s.abonentsItem}>
+                    <NavLink to="/dialogs/5"> Victor</NavLink>
+                </div>
+                <div className={s.abonentsItem}>
+                    <NavLink to="/dialogs/6"> Valera</NavLink>
+                </div>
+
             </div>
 
-        <div className={s.messages}>
+            <div className={s.messages}>
                 <div className={s.messagesItem}> Hi</div>
                 <div className={s.messagesItem}> How are you?</div>
                 <div className={s.messagesItem}> How its going?</div>
