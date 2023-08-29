df36ee8e291f8b123b20730e7c79400f4a713a19
Dmytro <dmii3iy@gmail.com>
2023-08-29 14:34:27 +0300

    Data from myPosts to Profile

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,11 +2,11 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post'
 const MyPosts = (props) => {
 
-  let postsData=[
-    {id:1, message: "Hi, How are you?", likeCounts:15},
-    {id:2, message: "It's my first message", likeCounts:20}]
+  // let postsData=[
+  //   {id:1, message: "Hi, How are you?", likeCounts:15},
+  //   {id:2, message: "It's my first message", likeCounts:20}]
      
-    let postsmodifData=postsData.map(pst=> <Post message={pst.message} 
+    let postsmodifData=props.postsData.map(pst=> <Post message={pst.message} 
       likeCounts={pst.likeCounts} />)
 
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,10 +2,13 @@ import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
-const Profile = () => {
+const Profile = (props) => {
+  let postsData=[
+    {id:1, message: "Hi, How are you?", likeCounts:15},
+    {id:2, message: "It's my first message", likeCounts:20}]
   return <div >
     <ProfileInfo />
-    <MyPosts />
+    <MyPosts postsData = {props.postsData}/>
   </div>
 }
 export default Profile
